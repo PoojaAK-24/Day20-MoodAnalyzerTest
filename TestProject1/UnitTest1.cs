bc80cf3336dcb3d5f3bd477f6f3cb378c1f67426
PoojaAK-24 <85796760+PoojaAK-24@users.noreply.github.com>
2021-07-28 16:16:04 +0530

    Pooja Added:To Return Mood For A String

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -10,18 +10,19 @@ namespace TestProject1
         [TestMethod]
         public void GivenMoodHappy_ShouldReturnHappy()
         {
-            MoodAnalyser obj = new MoodAnalyser("HAPPY");
+            MoodAnalyser obj = new MoodAnalyser("I am in happy mood");
             string result = obj.analyseMood();
-            Assert.AreEqual("HAPPY", result);
+            Assert.AreEqual("SAD", result);
         }
 
         [TestMethod]
         public void GivenMoodSad_ShouldReturnSad()
         {
-            MoodAnalyser obj = new MoodAnalyser("sad");
+            MoodAnalyser obj = new MoodAnalyser("I am in sad mood ");
             string result = obj.analyseMood();
             Assert.AreEqual("SAD", result);
         }
+
     }
 
 }
